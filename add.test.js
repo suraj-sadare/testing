7b33cc63c1b4850e492215f4625970bde2e58232
suraj sadare <sadaresuraj@gmail.com>
2021-08-03 17:58:09 +0530

    Testing

---
+++ add.test.js
@@ -0,0 +1,17 @@
+const sum = require('./add');
+
+test('It should be a proper addition',()=>{
+    expect(sum(1,2)).toBe(3)
+})
+test('It should be add two char',()=>{
+    expect(sum('a','b')).toBe('ab')
+})
+test('It should add two string',()=>{
+    expect(sum('Jivan','Ghadage')).toBe('JivanGhadage')
+})
+test('It should be a proper addition with defferent sign',()=>{
+    expect(sum(-3,+4)).toBe(1)
+})
+test('It should be a proper addition',()=>{
+    expect(sum(-3,+4)).toBe(1)
+})
