1765e60336e883a5ed45721749b3598ff845089d
suraj sadare <sadaresuraj@gmail.com>
2021-08-03 23:53:04 +0530

    Extended function to Api

--- add.js
+++ add.js
@@ -1,4 +1,16 @@
-function sum (a,b){
-    return a+b;
-}
-module.exports = sum;
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+let a=10,b=20;
+const c =a+b;
+
+app.get("/", (req, res) => {
+  return res.json(c);
+});
+
+const port = 8080;
+module.exports = app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+});
--- add.test.js
+++ add.test.js
@@ -1,17 +1,10 @@
-const sum = require('./add');
 
-test('It should be a proper addition',()=>{
-    expect(sum(1,2)).toBe(3)
-})
-test('It should be add two char',()=>{
-    expect(sum('a','b')).toBe('ab')
-})
-test('It should add two string',()=>{
-    expect(sum('Jivan','Ghadage')).toBe('JivanGhadage')
-})
-test('It should be a proper addition with defferent sign',()=>{
-    expect(sum(-3,+4)).toBe(1)
-})
-test('It should return string as result',()=>{
-    expect(sum(3,'a')).toBe('3a')
-})
+const request = require("supertest");
+const app = require('./add');
+describe("GET / ", () => {
+    test("It should respond with an array of students", async () => {
+      const response = await request(app).get("/");
+      expect(response.body).toEqual(30);
+      expect(response.statusCode).toBe(200);
+    });
+  });
